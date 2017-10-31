cebe8f979976d9a5f38140095988eab70e429359
arputharajraja <developer@gsche-wkst01.kaaysystems.com>
2017-10-31 19:52:07 +0530

    changes added

--- app/src/main/java/com/example/sample/SecondActivity.kt
+++ app/src/main/java/com/example/sample/SecondActivity.kt
@@ -8,6 +8,7 @@ import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
--- app/src/main/java/com/example/sample/ThirdActivity.kt
+++ app/src/main/java/com/example/sample/ThirdActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class ThirdActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
